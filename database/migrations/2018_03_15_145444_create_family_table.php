a73197324a99a64a6b84870c13a037ef19357dd2
kazuanzo <coicoikitakita@softbank.ne.jp>
2018-09-10 07:02:18 +0900

    first commit

---
+++ database/migrations/2018_03_15_145444_create_family_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFamilyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *もし同じ家族名の家族がいたら家族名の隣に重複しない数字を追加
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('family', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('family_name');
+            $table->integer('people_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('family');
+    }
+}
